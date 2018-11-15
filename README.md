cf7a0d2a0ee416a373a16e212380c6d273117acc
Luka komp <lukazrnic5@gmail.com>
2018-11-15 21:46:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+:envelope: Newsletter Template :envelope:
+===
+
+File List
+---
+```
+./public
+
+./views
+
+app.js
+
+schema.sql
+
+README.md
+```
+
+Description
+---
+Landing page for sign up to newsletter.
+All entered emails will be saved into "users" table in join_us database (which you need to create - read Installation instructions).
+
+**Code in *app.js* file from line 38 to line 50 will create 10 test users.**
+Simply delete that part of the code if you do not need it.
+
+Created using NodeJS and MySql.
+
+Installation
+---
+1. Donwload or clone the project
+2. Go into the project
+3. Run `npm install`
+4. Create database and name it "join_us"
+5. Set username and password in **app.js** -> **line 13 and 14**
+6. Run `node app.js`
+
+Application will run on localhost:8080.
+
+Screenshot
+---
+
---
+++ app.js
@@ -0,0 +1,81 @@
+var faker = require('faker');
+var express = require("express");
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+var connection = mysql.createConnection({
+    host    : 'localhost',
+    user    : 'root',
+    password: '',
+    database: 'join_us',
+    timezone: 'UTC+1'
+});
+
+// Creating user table
+connection.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+
+    let createUsers = `create table if not exists users(
+                            email VARCHAR(255) PRIMARY KEY,
+                            created_at TIMESTAMP DEFAULT NOW()
+                      )`;
+
+    connection.query(createUsers, function (err, results, fields) {
+        if (err) {
+            console.log(err.message);
+        }
+    });
+
+    // Inserting some fake data in users table - from line 38 to line 50
+    // delete from line 38 to line 50 if you want empty users table
+    var data = [];
+    for (var i = 0; i < 10; i++) {
+        data.push([
+            faker.internet.email(),
+            faker.date.past()
+        ]);
+    };
+
+    var q = 'INSERT INTO users (email, created_at) VALUES ?';
+    connection.query(q, [data], function (error, results) {
+        if (error) throw error;
+        console.log(results);
+    });
+
+    // connection.end(function (err) {
+    //     if (err) {
+    //         return console.log(err.message);
+    //     }
+    // });
+});
+
+app.get('/', function(req, res){
+    var q = 'SELECT COUNT(*) AS count FROM users';
+    connection.query(q, function(err, results){
+        if (err) throw err;
+        var count = results[0].count;
+        res.render('home', {data: count});
+    });
+});
+
+app.post('/register', function(req, res){
+    var person = {
+        email: req.body.email
+    }
+    
+    connection.query('INSERT INTO users SET ?', person, function(err, result){
+        if (err) throw err;
+        res.redirect('/');
+    });
+});
+
+app.listen(8080, function(){
+    console.log('Server running on 8080!');
+});
